be5c4b7116ce67a24540c454946585d85de29783
RachelAlcraft <rachelalcraft@gmail.com>
2024-05-25 14:54:20 +0100

    Reformat

--- src/squice/DataLoaders.py
+++ src/squice/DataLoaders.py
@@ -7,7 +7,8 @@ import numpy as np
 
 ####################################################################################
 class DataLoader(ABC):
-    """Abstract class to load data from file and convert to a numpy matrix """    
+    """Abstract class to load data from file and convert to a numpy matrix"""
+
     def __init__(self, filename):
         self.filename = filename
         self.mtx = None
@@ -16,25 +17,29 @@ class DataLoader(ABC):
     def load(self):
         pass
 
+
 ####################################################################################
 class NumpyFile(DataLoader):
     """Class to load a file of numerical data from numpy serislisation format"""
-    
+
     def load(self):
-        """Load the numpy data """        
+        """Load the numpy data"""
         self.mtx = np.load(self.filename)
+
 
 ####################################################################################
 class ElectronDensityCcp4(DataLoader):
     """Class to load electron density in the ccp4 format"""
-    
+
     def load(self):
-        """Load the numpy data """
+        """Load the numpy data"""
         self.mtx = None
+
+
 ####################################################################################
 
 if __name__ == "__main__":
-    #a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) #shape (3x3)
-    #np.save('speed_data.npy', a)                
+    # a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) #shape (3x3)
+    # np.save('speed_data.npy', a)
     npf = NumpyFile("speed_data.npy")
     npf.load()
